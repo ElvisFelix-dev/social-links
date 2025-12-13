ad0e71837511c5dd5e20d0a17f6b211224bebcea
Elvis Felix <elvisjaspion@gmail.com>
2025-12-13 17:26:39 -0300

    update userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,22 @@
 import { Router } from 'express'
+import passport from 'passport'
 import { googleLogin } from '../controllers/userController.js'
 
 const router = Router()
 
-router.post('/google', googleLogin)
+// 🔐 Inicia login com Google
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+// 🔁 Callback do Google
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { session: false }),
+  googleLogin
+)
 
 export default router

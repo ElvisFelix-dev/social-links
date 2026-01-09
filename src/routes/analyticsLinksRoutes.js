e37bce4cf37d4decc3b7e0df4a519764e5425180
Elvis Felix <elvisjaspion@gmail.com>
2026-01-08 22:00:10 -0300

    update

--- src/routes/analyticsLinksRoutes.js
+++ src/routes/analyticsLinksRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getTopClickedLinks, getClicksOverview } from '../controllers/analyticsLinksController.js'
+import { getTopClickedLinks, getOverview } from '../controllers/analyticsLinksController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
 
 const router = express.Router()
@@ -7,6 +7,6 @@ const router = express.Router()
 router.use(authMiddleware)
 
 router.get('/analytics-links', getTopClickedLinks)
-router.get('/clicks-overview', getClicksOverview)
+router.get('/clicks-overview', getOverview)
 
 export default router
